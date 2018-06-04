35836853b40020327492a1b15b4205d86fdf30da
Ricardo Delgado <ricdelgado80@gmail.com>
2018-06-04 17:04:30 -0400

    db correction

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://rdelgado80:Eckpfs96$$@ds023704.mlab.com:23704/vidjot-prod'
+    mongoURI: 'mongodb://rdelga80:Eckpfs96E@ds023704.mlab.com:23704/vidjot-prod'
   }
 } else {
   module.exports = {
